85faaf4bf234cdb5a036415a3eb3fbbfe6d678ab
rosulucian <rosu.lucian@gmail.com>
2019-02-21 11:48:34 +0700

    refactor transcoder

--- transcoder.go
+++ transcoder.go
@@ -4,59 +4,45 @@ import (
 	"context"
 	"math/big"
 
-	"github.com/VideoCoin/go-protocol/abis/stream"
 	"github.com/VideoCoin/go-videocoin/accounts/abi/bind"
-	"github.com/VideoCoin/go-videocoin/common"
-	"github.com/VideoCoin/go-videocoin/ethclient"
 )
 
 // TranscoderClient is a wrapper for the transcoder calls.
 type TranscoderClient struct {
-	StreamContract
+	ManagerContract
 	Caller
 }
 
 // NewTranscoderClient creates a TranscoderClient instance
 func NewTranscoderClient(url string, addr string, keyfilePath string, pwd string) (*TranscoderClient, error) {
-	streamAddress := common.HexToAddress(addr)
-
-	client, err := ethclient.Dial(url)
+	contract, caller, err := NewManagerContract(url, addr, keyfilePath, pwd)
 	if err != nil {
 		return nil, err
 	}
 
-	caller := Caller{client: client}
+	c := &TranscoderClient{*contract, *caller}
 
-	err = caller.loadAccount(keyfilePath, pwd)
-	if err != nil {
-		return nil, err
-	}
-
-	instance, err := stream.NewStream(streamAddress, client)
-	if err != nil {
-		return nil, err
-	}
-
-	contract := StreamContract{instance, streamAddress}
-
-	t := &TranscoderClient{contract, caller}
-
-	return t, nil
+	return c, nil
 }
 
 // SubmitProof sends
-func (t *TranscoderClient) SubmitProof(ctx context.Context, bitrate *big.Int, inputChunkID *big.Int, outputChunkID *big.Int) error {
-	opt := t.getTxOptions(0)
-	proof := big.NewInt(0)
-
-	// TODO: add checks
-
-	tx, err := t.instance.SubmitProof(opt, bitrate, inputChunkID, proof, outputChunkID)
+func (c *TranscoderClient) SubmitProof(ctx context.Context, streamID *big.Int, bitrate *big.Int, inputChunkID *big.Int, outputChunkID *big.Int) error {
+	stream, err := c.GetStreamContract(streamID, c.client)
 	if err != nil {
 		return err
 	}
 
-	_, err = bind.WaitMined(ctx, t.client, tx)
+	proof := big.NewInt(0)
+
+	// TODO: add checks
+
+	opt := c.getTxOptions(0)
+	tx, err := stream.instance.SubmitProof(opt, bitrate, inputChunkID, proof, outputChunkID)
+	if err != nil {
+		return err
+	}
+
+	_, err = bind.WaitMined(ctx, c.client, tx)
 	if err != nil {
 		return err
 	}
